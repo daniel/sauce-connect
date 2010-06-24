35758f3fb71c55b508ba597cdbcafd816c867027
jeremy avnet <brainsik@saucelabs.com>
2010-06-24 15:50:20 -0700

    Creates a new tunnel machine with a reverse SSH connection. MVP.

--- icicle.py
+++ icicle.py
@@ -2,12 +2,15 @@
 # encoding: utf-8
 from __future__ import with_statement
 
+# BUG: !! accidentally used Python 2.6 str.format all over :-/
+
 import os
 import re
 import optparse
 import subprocess
 import httplib
 import urllib2
+import time
 from contextlib import closing
 
 try:
@@ -18,74 +21,107 @@ except ImportError:
 from pprint import pprint
 
 
-class TunnelREST(object):
+class TunnelMachine(object):
 
     _host_search = re.compile("//([^/]+)").search
 
-    def __init__(self, rest_url, user, password):
+    def __init__(self, rest_url, user, password, domains):
         self.user = user
+        self.domains = set(domains)
         self.base_url = "{0[rest_url]}/{0[user]}/tunnels".format(locals())
+        self.rest_host = self._host_search(rest_url).group(1)
+        self.auth_header = dict(Authorization="Basic " + "{0}:{1}"
+                                "".format(user,password).encode("base64"))
+        self._set_urlopen(rest_url, user, password)
+        self._start_tunnel()
 
-        match = self._host_search(rest_url)
-        self.host = match.group(1)
-
-        auth_enc = "{0[user]}:{0[password]}".format(locals()).encode("base64")
-        self.auth_header = dict(Authorization="Basic %s" % auth_enc)
-
-        # make http auth support Just Work (for GET/POST)
+    def _set_urlopen(self, url, user, password):
+        # make http auth support Just Work for GET/POST
         password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        password_mgr.add_password(realm=None, uri=rest_url,
+        password_mgr.add_password(realm=None, uri=url,
                                   user=user, passwd=password)
         auth_handler = urllib2.HTTPBasicAuthHandler(password_mgr)
         opener = urllib2.build_opener(auth_handler)
-        urllib2.install_opener(opener)
+        self.urlopen = opener.open
 
-    def _create(self, url, domains):
-        json_header = {"Content-Type": "application/json"}
-        data = json.dumps(dict(DomainNames=domains))
-        req = urllib2.Request(url=url, headers=json_header, data=data)
-        with closing(urllib2.urlopen(req)) as resp:
+    def _get_doc(self, url_or_req):
+        with closing(self.urlopen(url_or_req)) as resp:
+            # TODO: handle this error
+            assert resp.msg == "OK"
             return json.loads(resp.read())
 
-    # TODO: ?? add a flag for or keep these connections persistent?
-    def _read(self, url):
-        with closing(urllib2.urlopen(url)) as resp:
-            return json.loads(resp.read())
-
-    def _delete(self, url):
+    def _get_delete_doc(self, url):
         if self.base_url.startswith("https"):
             make_conn = httplib.HTTPSConnection
         else:
             make_conn = httplib.HTTPConnection
-        with closing(make_conn(self.host)) as conn:
+        with closing(make_conn(self.rest_host)) as conn:
             conn.request(method="DELETE", url=url, headers=self.auth_header)
+            # TODO: check HTTP OK
             resp = conn.getresponse()
             return json.loads(resp.read())
 
-    def start_tunnel(self, domains):
-        return self._create(self.base_url, domains)
+    def _start_tunnel(self):
+        # Shutdown any tunnel using a requested domain
+        kill_list = set()
+        for doc in self._get_doc(self.base_url):
+            if not doc.get('DomainNames'):
+                continue
+            if set(doc['DomainNames']) & self.domains:
+                kill_list.add(doc['id'])
+        if kill_list:
+            print "Killing running tunnel(s) using requested domains:"
+        for tunnel_id in kill_list:
+            print "  Shutting down tunnel: %s" % tunnel_id
+            url = "%s/%s" % (self.base_url, tunnel_id)
+            doc = self._get_delete_doc(url)
+            assert doc.get('ok')  # TODO: handle error
 
-    def get_tunnels(self, tunnel_id=None):
-        url = self.base_url
-        if tunnel_id:
-            url += "/%s" % tunnel_id
-        return self._read(url)
+        # Request a tunnel machine
+        headers = {"Content-Type": "application/json"}
+        data = json.dumps(dict(DomainNames=list(self.domains)))
+        req = urllib2.Request(url=self.base_url, headers=headers, data=data)
+        doc = self._get_doc(req)
+        # TODO: handle this error
+        assert doc.get('ok')
+        self.id = doc['id']  # TODO: handle this not existing (fail)
+        self.url = "%s/%s" % (self.base_url, self.id)
 
-    def shutdown_tunnel(self, tunnel_id):
-        url = self.base_url + "/%s" % tunnel_id
-        return self._delete(url)
+        # Wait for the machine to start running
+        while True:
+            doc = self._get_doc(self.url)
+            status = doc.get('Status')
+            # TODO: error on halting, terminated, etc.
+            if status == "running":
+                break
+            print u"Tunnel is %s …" % status
+            time.sleep(5)
+        self.host = doc['Host']
+        print "Tunnel is running on %s!" % self.host
+
+    def shutdown(self):
+        doc = self._get_delete_doc(self.url)
+        assert doc.get('ok')
+        while True:
+            doc = self._get_doc(self.url)
+            status = doc.get('Status')
+            if status == "terminated":
+                break
+            print u"Tunnel is %s …" % status
+            time.sleep(5)
+        print "Tunnel is shutdown!"
 
 
-def get_expect_script(options):
+def get_expect_script(options, remote_host):
     return ";".join("""set timeout -1
-spawn ssh-keygen -R {0.remote_host}
-spawn ssh -p 22 -N -R 0.0.0.0:{0.remote_port}:{0.host}:{0.port} {0.remote_host}
+spawn ssh-keygen -R {1}
+spawn ssh -p 22 -N -R 0.0.0.0:{0.remote_port}:{0.host}:{0.port} {1}
 expect \\"Are you sure you want to continue connecting (yes/no)?\\"
 send -- yes\\r
 expect *password:
 send -- {0.api_key}\\r
 interact
-""".format(options).split("\n"))
+""".format(options, remote_host).split("\n"))
 
 
 def get_options():
@@ -106,7 +142,6 @@ def get_options():
                   help="default: %default")
 
     (options, args) = op.parse_args()
-    options.remote_host = "maki4201.miso.saucelabs.com"
     for opt in ["user", "api_key", "host", "port", "domains"]:
         if not hasattr(options, opt) or not getattr(options, opt):
             op.error("Missing required argument(s)!")
@@ -118,13 +153,9 @@ def get_options():
 
 def main():
     options = get_options()
-    rest = TunnelREST(options.rest_url, options.user, options.api_key)
-
-    pprint(rest.get_tunnels())
-    pprint(rest.start_tunnel(options.domains))
-
-    raise SystemExit()
-    script = get_expect_script(options)
+    tunnel = TunnelMachine(options.rest_url, options.user, options.api_key,
+                           options.domains)
+    script = get_expect_script(options, tunnel.host)
     subprocess.call('exec expect -c "%s"' % script, shell=True,
                     stdout=open(os.devnull))
 
