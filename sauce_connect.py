0042f1a322206f090f95b7e3755804aeb5583aa2
jeremy avnet <brainsik@saucelabs.com>
2010-11-18 12:28:38 -0800

    Bumping release number

--- sauce_connect.py
+++ sauce_connect.py
@@ -39,7 +39,7 @@ except ImportError:
     import simplejson as json  # Python 2.5 dependency
 
 NAME = "sauce_connect"
-RELEASE = 19
+RELEASE = 20
 DISPLAY_VERSION = "%s release %s" % (NAME, RELEASE)
 PRODUCT_NAME = u"Sauce Connect"
 VERSIONS_URL = "http://saucelabs.com/versions.json"
